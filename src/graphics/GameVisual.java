4c23a4d7d06795565e776c525b014fd0819d5145
panda <daniel.phan36@gmail.com>
2017-05-04 09:47:03 -0700

    initialized graphics components (GameVisual.java)

--- src/graphics/GameVisual.java
+++ src/graphics/GameVisual.java
@@ -1,6 +1,7 @@
 package graphics;
 
 import entities.Direction;
+import entities.Enemy;
 import entities.EnemyFactory;
 import entities.Pacman;
 import maze.Game;
@@ -8,8 +9,12 @@ import maze.MazeGenerator;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Daniel Phan
@@ -23,35 +28,9 @@ public class GameVisual extends Game {
         super(generator, player, enemyFactory, goalCount);
         frame_ = new JFrame(title);
         currentArrowEvent_ = null;
-
-        frame_.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                //Nothing
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (isArrow(e)) {
-                    currentArrowEvent_ = e;
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (isArrow(e)) {
-                    currentArrowEvent_ = null;
-                }
-            }
-
-            private boolean isArrow(KeyEvent e) {
-                int keycode = e.getKeyCode();
-                return keycode == KeyEvent.VK_UP
-                    || keycode == KeyEvent.VK_DOWN
-                    || keycode == KeyEvent.VK_LEFT
-                    || keycode == KeyEvent.VK_RIGHT;
-            }
-        });
+        initComponents();
+        initFrame();
+        initArrowListener();
     }
 
     public void run() {
@@ -84,5 +63,69 @@ public class GameVisual extends Game {
 
     private JFrame frame_;
     private KeyEvent currentArrowEvent_;
+    private MazeGenVisual vGenerator_;
+    private EntityVisual vPlayer_;
+    private List<EntityVisual> vEnemies_;
     private static final int UPDATE_DELAY = 100;
+    private static final String PLAYER_IMG = "pacman.gif";
+    private static final String[] ENEMY_IMGs = {
+        "redghost.gif", "purpleghost.gif", "blueghost.gif"
+    };
+
+    private void initFrame() {
+        frame_.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Dimension contentDimensions = new Dimension(
+            generator().maze()[0].length * MazeGenVisual.CELL_LENGTH,
+            generator().maze().length * MazeGenVisual.CELL_LENGTH);
+        frame_.getContentPane().setPreferredSize(contentDimensions);
+        frame_.pack();
+
+        frame_.add(vGenerator_);
+        frame_.add(vPlayer_);
+        vEnemies_.forEach(frame_::add);
+
+        frame_.setVisible(true);
+    }
+
+    private void initComponents() {
+        vGenerator_ = new MazeGenVisual(generator());
+        vPlayer_ = new EntityVisual(player(), PLAYER_IMG, MazeGenVisual.CELL_LENGTH);
+
+        vEnemies_ = new LinkedList<>();
+        for (Enemy e : enemies()) {
+            String img = ENEMY_IMGs[(int)(Math.random() * ENEMY_IMGs.length)];
+            vEnemies_.add(new EntityVisual(e, img, MazeGenVisual.CELL_LENGTH));
+        }
+    }
+
+    private void initArrowListener() {
+        frame_.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //Nothing
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (isArrow(e)) {
+                    currentArrowEvent_ = e;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (isArrow(e)) {
+                    currentArrowEvent_ = null;
+                }
+            }
+
+            private boolean isArrow(KeyEvent e) {
+                int keycode = e.getKeyCode();
+                return keycode == KeyEvent.VK_UP
+                    || keycode == KeyEvent.VK_DOWN
+                    || keycode == KeyEvent.VK_LEFT
+                    || keycode == KeyEvent.VK_RIGHT;
+            }
+        });
+    }
 }
