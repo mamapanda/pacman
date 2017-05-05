16dd598aef8644896e70f2ef08aace4802ae3c18
panda <daniel.phan36@gmail.com>
2017-05-04 18:24:21 -0700

    added Compile.ps1

---
+++ Compile.ps1
@@ -0,0 +1,11 @@
+param (
+    [Switch]$Run
+)
+
+$OutputDir = "out/Production/Maze"
+
+if ($Run) {
+    java -cp $OutputDir Program
+} else {
+    javac src/Program.java src/maze/*.java src/graphics/*.java src/entities/*.java -d $OutputDir
+}
